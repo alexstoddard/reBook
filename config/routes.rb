f59e84a81be523b98d37f113f8bb5c17174c3e11
Alex Stoddard <alex.stoddard@comcast.net>
2013-10-07 22:58:14 +0000

    hello world

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rebook::Application.routes.draw do
+  get "greetings/hello"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
