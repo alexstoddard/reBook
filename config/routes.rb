59014fe54650cd9d8cc389df33f897c971a6d18a
Alex Stoddard <alex.stoddard@comcast.net>
2013-10-10 15:19:34 +0000

    add some tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rebook::Application.routes.draw do
+  root 'greetings#hello'
   get "greetings/hello"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
