08f1dff8718b6b5a3c12b16d76684fbad874592b
Alex Stoddard <alex.stoddard@comcast.net>
2013-10-11 03:52:18 +0000

    add some fun to the hello world

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,5 +1,21 @@
+require 'rubygems'
+require 'json'
+require 'net/http'
+
 class GreetingsController < ApplicationController
   def hello
-     @message = "Hello, from reBook!"
+    @message = "Hello, from reBook!"
+    @result = search_google("Stewart Calculus")
+  end
+
+  def search_google(terms)
+    prefix = "https://www.googleapis.com/books/v1/volumes?q="
+    api_key = "&key=AIzaSyBiCf7WbyUGR8kFOD3fwTAOjWGSMOGx4fk"
+
+    search_string = prefix + terms + api_key
+    resp = Net::HTTP.get_response(URI.parse(URI.encode(search_string)))
+    data = resp.body
+
+    return JSON.parse(data)
   end
 end
