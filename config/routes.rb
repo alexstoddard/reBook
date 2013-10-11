514a52d1eae4888b83f22c9cc0eea8445efa5018
Alex Stoddard <alex.stoddard@comcast.net>
2013-10-11 01:28:20 +0000

    add some scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rebook::Application.routes.draw do
+  resources :users
+
   root 'greetings#hello'
   get "greetings/hello"
   # The priority is based upon order of creation: first created -> highest priority.
