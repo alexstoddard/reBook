fadfbd75d3ee940395a654a1fae24333b7c36efd
bitnami <afstrings@gmail.com>
2013-11-13 18:34:54 +0000

    Changed email validation link

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,15 +3,15 @@ class UserMailer < ActionMailer::Base
   
   def welcome_email(user)
     @user = user
-    @url  = 'http://localhost:3000/login?user_id='"#{@user.passhash}"
-    #@url  = 'rebook.herokuapp.com/login'
+    #@url  = 'http://localhost:3000/login?user_id='"#{@user.passhash}"
+    @url  = 'rebook.herokuapp.com/login?user_id='"#{@user.passhash}"
     mail(to: @user.email, subject: 'ReBook: Validate Email')
   end
 
   def reset_email(user)
     @user = user
-    @url  = 'http://localhost:3000/login_reset?user_id='"#{@user.passhash}"
-    #@url  = 'rebook.herokuapp.com/login'
+    #@url  = 'http://localhost:3000/login_reset?user_id='"#{@user.passhash}"
+    @url  = 'rebook.herokuapp.com/login_reset?user_id='"#{@user.passhash}"
     mail(to: @user.email, subject: 'ReBook: Reset Password')
   end
 end
