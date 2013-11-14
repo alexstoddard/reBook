ac9c73f85cf18422ca8f88e0764bcf001c109055
Alex Stoddard <alex.stoddard@comcast.net>
2013-11-14 18:51:31 +0000

    before environment change

--- app/models/inventory_need.rb
+++ app/models/inventory_need.rb
@@ -4,5 +4,8 @@ class InventoryNeed < ActiveRecord::Base
   belongs_to :book
   belongs_to :user
   belongs_to :condition
+  
+  has_many :own_matches, class_name: "InventoryOwn",
+                          foreign_key: "book_id", primary_key: "book_id"
 
 end
