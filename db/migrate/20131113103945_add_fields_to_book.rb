a0821a14a3d813485cba222ff391f382f1e392f8
Alex Stoddard <alex.stoddard@comcast.net>
2013-11-14 05:39:18 +0000

    change style

--- db/migrate/20131113103945_add_fields_to_book.rb
+++
@@ -1,7 +0,0 @@
-class AddFieldsToBook < ActiveRecord::Migration
-  def change
-    add_column :books, :isbn, :string
-    add_column :books, :published, :datetime
-    add_column :books, :description, :text
-  end
-end
