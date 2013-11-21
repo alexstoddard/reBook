4538edafde9fcedbac37ff0d16bceaf028f11184
Alex Stoddard <alex.stoddard@comcast.net>
2013-11-21 21:26:58 +0000

    refresh

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -145,7 +145,6 @@ class Trade < ActiveRecord::Base
     # Available 2 person trades
     x = InventoryOwn.where(:user_id => user).includes(:need_matches => {:user_owns => :need_matches })
 
-    debugger
 
     x.each do | o1 |
       o1.need_matches.each do | n1 |
