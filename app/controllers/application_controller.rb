7d21891d61f50911ef37d5bfc2e7d1f4215a40df
Alex Stoddard <alex.stoddard@comcast.net>
2013-11-23 01:52:27 +0000

    change application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :validate_user
+  #  before_filter :validate_user
 
   private
 
   def validate_user() 
     loginPage = ["/users"]
-    unrestricted = ["/users/new",
-					"/forgot"]
+    unrestricted = ["/users/new","/forgot"]
+
     if session[:user_id] != nil
       # continue to current_user url
 	  if session[:user_id] != 10 && loginPage.include?("/users")
