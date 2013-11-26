73ebe48979aade2a631de2dd7d7edc63155923fe
Alex Stoddard <alex.stoddard@comcast.net>
2013-11-26 13:15:05 +0000

    rspec info

--- spec/models/trade_spec.rb
+++ spec/models/trade_spec.rb
@@ -1,17 +1,43 @@
 require 'spec_helper'
 
 describe Trade do
-  it "each user should be in one trade line for own and one trade line for need" do
-    owns = {}
-    needs = {}
 
-    @trade = Trade.find(1)
-    
-    @trade.trade_lines.each do |line|
-      owns[line.inventory_own.user_id] += 1
-      needs[line.inventory_need.user_id] += 1
+  describe "Possible Trades" do
+    before :each do
+
+      @trades = []
+
+      User.all.collect { |x| x.id }.each do |y|
+        @trades += Trade.possible_trades(y)
+      end
+
     end
-   
-    
+
+    it "each user should be in one trade line for own and one trade line for need in possible trades" do
+
+      # For each trade count the number of times each user is in a hava and in a need
+      @trades.each do |trade|
+        owns = {}
+        needs = {}
+        
+        trade.trade_lines.each do |line|
+          if owns[line.inventory_own.user_id].nil?
+            owns[line.inventory_own.user_id] = 1
+          else
+            owns[line.inventory_own.user_id] += 1
+          end
+
+          if needs[line.inventory_need.user_id].nil?
+            needs[line.inventory_need.user_id] = 1
+          else
+            needs[line.inventory_need.user_id] += 1
+          end
+
+        end
+        
+        # Make sure that each user is in only one have and in one need
+        owns.keys.each { |x| needs[x].should eq(1) } and needs.keys.each { |x| owns[x].should eq(1) }
+      end
+    end
   end
 end
