a8f76b2388b1ca2fba2bd8c3601c7a817ba217ab
Alex Stoddard <alex.stoddard@comcast.net>
2013-12-05 17:33:51 +0000

    add fancybox gem

--- app/models/inventory_need.rb
+++ app/models/inventory_need.rb
@@ -5,13 +5,15 @@ class InventoryNeed < ActiveRecord::Base
   belongs_to :user
   belongs_to :condition
   
-  has_many :own_matches, class_name: "InventoryOwn",
-                          foreign_key: "book_id", 
-                          primary_key: "book_id"
+  has_many :own_matches, -> { where(deleted: false) },
+                            class_name: "InventoryOwn",
+                            foreign_key: "book_id", 
+                            primary_key: "book_id"
 
-  has_many :user_owns, class_name: "InventoryOwn",
-                       foreign_key: "user_id", 
-                       primary_key: "user_id"
+  has_many :user_owns, -> { where(deleted: false) },
+                          class_name: "InventoryOwn",
+                          foreign_key: "user_id", 
+                          primary_key: "user_id"
   
   has_many :trade_lines
   has_many :trades, through: :trade_lines
