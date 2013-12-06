3eb08cc9c3bf4999052cecd4bce3ba89ee7f762e
Alex Stoddard <alex.stoddard@comcast.net>
2013-12-06 05:04:08 +0000

    quick fix

--- db/scripts/reset_test.sh
+++
@@ -1,6 +0,0 @@
-#load 'db/seeds/herokutest.rb'
-#heroku config -r herokutest | grep POSTGRESQL
-#heroku run rails console -r herokutest
-heroku pg:reset HEROKU_POSTGRESQL_GRAY_URL --remote herokutest --confirm rebooktest
-heroku run rake db:migrate --remote herokutest
-heroku run rails runner "db/seeds/herokutest.rb" --remote herokutest
